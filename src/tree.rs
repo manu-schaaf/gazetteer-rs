a810ccdff39ee80d4605485fbac853a05e64b5c0
Manuel Stoeckel <manuel.stoeckel@em.uni-frankfurt.de>
2022-11-14 11:00:37 +0100

    feat+refactor: Changed to flat search map

--- src/tree.rs
+++ src/tree.rs
@@ -1,17 +1,15 @@
 use std::cmp::Ordering;
 use std::collections::{HashMap, HashSet};
-use std::collections::vec_deque::VecDeque;
 use std::fmt::{Display, Formatter};
 use std::hash::{Hash, Hasher};
 
 use indicatif::{ProgressBar, ProgressStyle};
 use itertools::Itertools;
 use rayon::prelude::*;
-use rocket::form::validate::Len;
 use rocket::FromFormField;
 use serde::{Deserialize, Serialize};
 
-use crate::util::{CorpusFormat, create_skip_grams, get_files, parse_files, Tokenizer};
+use crate::util::{create_skip_grams, get_files, parse_files, CorpusFormat, Tokenizer};
 
 #[derive(Debug, Serialize, Deserialize, FromFormField)]
 pub enum ResultSelection {
@@ -31,10 +29,10 @@ pub enum MatchType {
 impl MatchType {
     fn get_value(&self) -> i32 {
         match self {
-            MatchType::None => { -1 }
-            MatchType::Full => { 0 }
-            MatchType::Abbreviated => { 1 }
-            MatchType::SkipGram => { 2 }
+            MatchType::None => -1,
+            MatchType::Full => 0,
+            MatchType::Abbreviated => 1,
+            MatchType::SkipGram => 2,
         }
     }
 }
@@ -54,10 +52,18 @@ impl PartialOrd<Self> for MatchType {
 impl Display for MatchType {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         match self {
-            MatchType::None => { write!(f, "None") }
-            MatchType::Full => { write!(f, "Full") }
-            MatchType::Abbreviated => { write!(f, "Abbreviated") }
-            MatchType::SkipGram => { write!(f, "SkipGram") }
+            MatchType::None => {
+                write!(f, "None")
+            }
+            MatchType::Full => {
+                write!(f, "Full")
+            }
+            MatchType::Abbreviated => {
+                write!(f, "Abbreviated")
+            }
+            MatchType::SkipGram => {
+                write!(f, "SkipGram")
+            }
         }
     }
 }
@@ -77,7 +83,8 @@ impl Hash for Match {
 
 impl Ord for Match {
     fn cmp(&self, other: &Self) -> Ordering {
-        self.match_type.cmp(&other.match_type)
+        self.match_type
+            .cmp(&other.match_type)
             .then(self.match_string.cmp(&other.match_string))
             .then(self.match_label.cmp(&other.match_label))
     }
@@ -95,91 +102,69 @@ impl Display for Match {
     }
 }
 
-impl Match {
-    fn none() -> Self {
-        Self {
-            match_type: MatchType::None,
-            match_string: String::new(),
-            match_label: String::new(),
-        }
-    }
-
-    fn new(
-        match_type: MatchType,
-        match_string: String,
-        match_label: String,
-    ) -> Self {
-        Match {
-            match_type,
-            match_string,
-            match_label,
-        }
-    }
-
-    fn full(
-        match_string: String,
-        match_label: String,
-    ) -> Self {
-        Match {
-            match_type: MatchType::Full,
-            match_string,
-            match_label,
-        }
-    }
-}
-
 #[derive(Debug)]
 pub struct HashMapSearchTree {
-    pub matches: HashSet<Match>,
-    pub children: HashMap<String, HashMapSearchTree>,
-    tokenizer: Option<Tokenizer>,
+    pub search_map: HashMap<Vec<String>, HashSet<Match>>,
+    tokenizer: Tokenizer,
     tree_depth: usize,
 }
 
 impl HashMapSearchTree {
-    pub fn default() -> Self where Self: Sized {
+    pub fn default() -> Self
+    where
+        Self: Sized,
+    {
         HashMapSearchTree {
-            matches: HashSet::new(),
-            children: HashMap::new(),
-            tokenizer: Option::from(Tokenizer::default()),
+            search_map: HashMap::new(),
+            tokenizer: Tokenizer::default(),
             tree_depth: 0 as usize,
         }
     }
 
-    fn from(match_type: MatchType, match_string: String, match_label: String) -> Self {
-        Self {
-            matches: HashSet::from([Match::new(match_type, match_string, match_label)]),
-            children: HashMap::new(),
-            tokenizer: None,
-            tree_depth: 0 as usize,
-        }
-    }
-
-    fn child() -> Self {
-        HashMapSearchTree {
-            tokenizer: None,
-            ..HashMapSearchTree::default()
-        }
-    }
-
-    pub fn load_file(&mut self, root_path: &str, generate_skip_grams: bool, skip_gram_min_length: i32, skip_gram_max_skips: i32, filter_list: Option<&Vec<String>>, generate_abbrv: bool, format: &Option<CorpusFormat>) {
+    pub fn load_file(
+        &mut self,
+        root_path: &str,
+        generate_skip_grams: bool,
+        skip_gram_min_length: i32,
+        skip_gram_max_skips: i32,
+        filter_list: Option<&Vec<String>>,
+        generate_abbrv: bool,
+        format: &Option<CorpusFormat>,
+    ) {
         let files: Vec<String> = get_files(root_path);
         println!("Found {} files to read", files.len());
 
         let pb = ProgressBar::new(files.len() as u64);
-        pb.set_style(ProgressStyle::with_template(
-            "Loading Input Files {bar:40} {pos}/{len} {msg}"
-        ).unwrap());
-        let lines: Vec<(String, String)> = parse_files(files, Option::from(&pb), format, filter_list);
+        pb.set_style(
+            ProgressStyle::with_template("Loading Input Files {bar:40} {pos}/{len} {msg}").unwrap(),
+        );
+        let lines: Vec<(String, String)> =
+            parse_files(files, Option::from(&pb), format, filter_list);
         pb.finish_with_message("Done");
 
-        self.load(lines, generate_skip_grams, skip_gram_min_length, skip_gram_max_skips, generate_abbrv);
+        self.load(
+            lines,
+            generate_skip_grams,
+            skip_gram_min_length,
+            skip_gram_max_skips,
+            generate_abbrv,
+        );
     }
 
-    pub fn load(&mut self, entries: Vec<(String, String)>, generate_skip_grams: bool, skip_gram_min_length: i32, skip_gram_max_skips: i32, generate_abbrv: bool) {
+    pub fn load(
+        &mut self,
+        entries: Vec<(String, String)>,
+        generate_skip_grams: bool,
+        skip_gram_min_length: i32,
+        skip_gram_max_skips: i32,
+        generate_abbrv: bool,
+    ) {
         let search_terms: Vec<&str> = entries.iter().map(|line| line.0.as_str()).collect();
-        let segmented: Vec<(Vec<String>, Vec<(usize, usize)>)> = self.tokenize_batch(search_terms.as_slice()).unwrap();
-        let entries: Vec<(Vec<String>, String, String)> = segmented.into_iter().zip(entries.into_iter())
+        let segmented: Vec<(Vec<String>, Vec<(usize, usize)>)> =
+            self.tokenize_batch(search_terms.as_slice());
+        let entries: Vec<(Vec<String>, String, String)> = segmented
+            .into_iter()
+            .zip(entries.into_iter())
             .map(|(segments, (search_term, label))| (segments.0, search_term, label))
             .collect::<Vec<(Vec<String>, String, String)>>();
 
@@ -196,57 +181,70 @@ impl HashMapSearchTree {
 
     pub(crate) fn load_entries(&mut self, entries: &Vec<(Vec<String>, String, String)>) {
         let pb = ProgressBar::new(entries.len() as u64);
-        pb.set_style(ProgressStyle::with_template(
-            "Loading Entries {bar:40} {pos}/{len} {msg}"
-        ).unwrap());
+        pb.set_style(
+            ProgressStyle::with_template("Loading Entries {bar:40} {pos}/{len} {msg}").unwrap(),
+        );
 
         for (segments, search_term, label) in entries {
-            self.insert(VecDeque::from(segments.clone()), String::from(search_term), String::from(label), MatchType::Full);
+            self.insert(
+                segments.clone(),
+                String::from(search_term),
+                String::from(label),
+                MatchType::Full,
+            );
             pb.inc(1)
         }
         pb.finish_with_message("Done");
     }
 
-    pub fn insert(&mut self, mut values: VecDeque<String>, match_string: String, match_label: String, match_type: MatchType) {
-        if values.len() > self.tree_depth {
-            self.tree_depth = values.len() as usize;
+    pub fn insert(
+        &mut self,
+        segments: Vec<String>,
+        match_string: String,
+        match_label: String,
+        match_type: MatchType,
+    ) {
+        if segments.len() > self.tree_depth {
+            self.tree_depth = segments.len() as usize;
         }
 
-        if let Some(value) = values.pop_front() {
-            let value = value;
-            match self.children.get_mut(&value) {
-                Some(mut child) => {
-                    if values.is_empty() {
-                        child.matches.insert(Match { match_type, match_string, match_label });
-                    } else {
-                        child.insert(values, match_string, match_label, match_type);
-                    }
-                }
-                None => {
-                    if values.is_empty() {
-                        self.children.insert(value, HashMapSearchTree::from(match_type, match_string, match_label));
-                    } else {
-                        match self.children.try_insert(value, HashMapSearchTree::child()) {
-                            Ok(child) => { child.insert(values, match_string, match_label, match_type); }
-                            Err(err) => { panic!("{:?}", err) }
-                        }
-                        // let mut child = self.children.insert(value, HashMapSearchTree::default()).expect("!");
-                        // child.insert(values, match_string, match_uri, match_type);
-                    }
-                }
+        match self.search_map.get_mut(&segments) {
+            Some(search_result) => {
+                search_result.insert(Match {
+                    match_type,
+                    match_string,
+                    match_label,
+                });
+            }
+            None => {
+                self.search_map.insert(
+                    segments,
+                    HashSet::from([Match {
+                        match_type,
+                        match_string,
+                        match_label,
+                    }]),
+                );
             }
         }
     }
 
-    pub(crate) fn generate_skip_grams(&mut self, lines: &Vec<(Vec<String>, String, String)>, min_length: i32, max_skips: i32) {
-        let filtered = lines.par_iter()
+    pub(crate) fn generate_skip_grams(
+        &mut self,
+        lines: &Vec<(Vec<String>, String, String)>,
+        min_length: i32,
+        max_skips: i32,
+    ) {
+        let filtered = lines
+            .par_iter()
             .filter(|(segments, _, _)| segments.len() > min_length as usize)
             .collect::<Vec<_>>();
 
         let pb = ProgressBar::new(filtered.len() as u64);
-        pb.set_style(ProgressStyle::with_template(
-            "Generating skip-grams {bar:40} {pos}/{len} {msg}"
-        ).unwrap());
+        pb.set_style(
+            ProgressStyle::with_template("Generating skip-grams {bar:40} {pos}/{len} {msg}")
+                .unwrap(),
+        );
 
         let mut counter: i64 = 0;
         for (segments, search_term, label) in filtered {
@@ -255,7 +253,7 @@ impl HashMapSearchTree {
             deletes.dedup();
             for skip_gram in deletes {
                 self.insert(
-                    VecDeque::from(skip_gram),
+                    skip_gram.clone(),
                     String::from(search_term),
                     String::from(label),
                     MatchType::SkipGram,
@@ -268,14 +266,16 @@ impl HashMapSearchTree {
     }
 
     pub(crate) fn generate_abbreviations(&mut self, lines: &Vec<(Vec<String>, String, String)>) {
-        let filtered = lines.par_iter()
+        let filtered = lines
+            .par_iter()
             .filter(|(segments, _, _)| segments.len() > 1)
             .collect::<Vec<_>>();
 
         let pb = ProgressBar::new(filtered.len() as u64);
-        pb.set_style(ProgressStyle::with_template(
-            "Generating Abbreviations {bar:40} {pos}/{len} {msg}"
-        ).unwrap());
+        pb.set_style(
+            ProgressStyle::with_template("Generating Abbreviations {bar:40} {pos}/{len} {msg}")
+                .unwrap(),
+        );
 
         let mut counter: i64 = 0;
         let mut abbrv: Vec<String> = Vec::new();
@@ -291,7 +291,12 @@ impl HashMapSearchTree {
                 abbrv.push(abbreviated_segment);
                 abbrv.extend_from_slice(&segments[(i + 1)..]);
 
-                self.insert(VecDeque::from(abbrv.clone()), String::from(search_term), String::from(label), MatchType::Abbreviated);
+                self.insert(
+                    abbrv.clone(),
+                    String::from(search_term),
+                    String::from(label),
+                    MatchType::Abbreviated,
+                );
                 counter += 1;
             }
             pb.inc(1);
@@ -300,42 +305,24 @@ impl HashMapSearchTree {
         pb.finish_with_message(format!("Generated {} abbreviated entries", counter));
     }
 
-    pub(crate) fn tokenize(&self, input: &str) -> Result<(Vec<String>, Vec<(usize, usize)>), String> {
-        match &self.tokenizer {
-            Some(tokenizer) => {
-                Ok(tokenizer.tokenize(input))
-            }
-            None => {
-                Err(String::from("Missing tokenizer!"))
-            }
-        }
+    pub(crate) fn tokenize(&self, input: &str) -> (Vec<String>, Vec<(usize, usize)>) {
+        self.tokenizer.tokenize(input)
     }
 
-    pub(crate) fn tokenize_batch(&self, input: &[&str]) -> Result<Vec<(Vec<String>, Vec<(usize, usize)>)>, String> {
-        match &self.tokenizer {
-            Some(tokenizer) => {
-                Ok(tokenizer.encode_batch(input))
-            }
-            None => {
-                Err(String::from("Missing tokenizer!"))
-            }
-        }
+    pub(crate) fn tokenize_batch(&self, input: &[&str]) -> Vec<(Vec<String>, Vec<(usize, usize)>)> {
+        self.tokenizer.encode_batch(input)
     }
 
-    pub(crate) fn traverse(&self, values: VecDeque<String>) -> Result<Vec<(Vec<String>, &HashSet<Match>)>, String> {
-        let vec = self.traverse_internal(values, Vec::new(), Vec::new());
-        if vec.len() > 0 {
-            Ok(vec)
-        } else {
-            Err(String::from("No matches found"))
-        }
-    }
-
-    pub fn search<'a>(&self, text: &'a str, max_len: Option<usize>, result_selection: Option<&ResultSelection>) -> Vec<(String, Vec<Match>, usize, usize)> {
+    pub fn search<'a>(
+        &self,
+        text: &'a str,
+        max_len: Option<usize>,
+        result_selection: Option<&ResultSelection>,
+    ) -> Vec<(String, Vec<Match>, usize, usize)> {
         let result_selection = result_selection.unwrap_or(&ResultSelection::LastPreferFull);
         let max_len = max_len.unwrap_or(self.tree_depth as usize);
 
-        let (mut slices, mut offsets) = self.tokenize(text).unwrap();
+        let (mut slices, mut offsets) = self.tokenize(text);
 
         // Pad the slices and their offsets to include the last words
         slices.extend(vec![String::new(); max_len]);
@@ -344,10 +331,22 @@ impl HashMapSearchTree {
 
         let mut results = slices
             .par_windows(max_len)
-            .map(|slice| self.traverse(VecDeque::from(slice.to_vec())))
+            .map(|slice| self.traverse(slice.to_vec()))
             .zip(offsets.par_windows(max_len))
-            .filter_map(|(result, offsets)| if let Ok(result) = result { Some((result, offsets)) } else { None })
-            .filter_map(|(result, offsets)| if !result.is_empty() { Some((result, offsets)) } else { None })
+            .filter_map(|(result, offsets)| {
+                if let Ok(result) = result {
+                    Some((result, offsets))
+                } else {
+                    None
+                }
+            })
+            .filter_map(|(result, offsets)| {
+                if !result.is_empty() {
+                    Some((result, offsets))
+                } else {
+                    None
+                }
+            })
             .map(|(results, offsets)| {
                 let start = offsets[0].0;
                 match result_selection {
@@ -367,12 +366,18 @@ impl HashMapSearchTree {
                     ResultSelection::LastPreferFull => {
                         let result = results.last().unwrap();
                         let end = offsets[result.0.len() - 1].1;
-                        if result.1.iter().any(|mtch| mtch.match_type == MatchType::Full) {
+                        if result
+                            .1
+                            .iter()
+                            .any(|mtch| mtch.match_type == MatchType::Full)
+                        {
                             let mut _matches = HashSet::new();
-                            _matches.extend(result.1.iter().filter_map(|mtch| if mtch.match_type == MatchType::Full {
-                                Some(mtch.clone())
-                            } else {
-                                None
+                            _matches.extend(result.1.iter().filter_map(|mtch| {
+                                if mtch.match_type == MatchType::Full {
+                                    Some(mtch.clone())
+                                } else {
+                                    None
+                                }
                             }));
                             return vec![(result.0.join(" "), _matches, start, end)];
                         }
@@ -391,29 +396,21 @@ impl HashMapSearchTree {
         results
     }
 
-    fn traverse_internal<'a>(
-        &'a self,
-        mut values: VecDeque<String>,
-        mut matched_string_buffer: Vec<String>,
-        mut results: Vec<(Vec<String>, &'a HashSet<Match>)>,
-    ) -> Vec<(Vec<String>, &'a HashSet<Match>)> {
-        let value = values.pop_front().expect("");
-        match self.children.get(&value) {
-            Some(child) => {
-                matched_string_buffer.push(value.to_string());
-                if !child.matches.is_empty() {
-                    results.push((matched_string_buffer.clone(), &child.matches));
-                }
-
-                if !values.is_empty() {
-                    child.traverse_internal(values, matched_string_buffer, results)
-                } else {
-                    results
-                }
+    pub(crate) fn traverse(
+        &self,
+        window: Vec<String>,
+    ) -> Result<Vec<(Vec<String>, &HashSet<Match>)>, String> {
+        let mut results = Vec::new();
+        for i in 1..window.len() {
+            let sub_window = window[0..=i].to_vec();
+            if let Some(result) = self.search_map.get(&sub_window) {
+                results.push((sub_window, result));
             }
-            None => {
-                results
-            }
+        }
+        if results.len() > 0 {
+            Ok(results)
+        } else {
+            Err(String::from("No matches found"))
         }
     }
 }
@@ -433,6 +430,8 @@ mod test {
         ];
         tree.load(entries.clone(), false, 0, 0, false);
         let tree = tree;
+
+        println!("{:?}", tree.search_map);
 
         let results = tree.search("An xyz", Some(3), None);
         assert!(results.is_empty());
@@ -462,7 +461,12 @@ mod test {
         println!("{:?}", results);
         let matches: Vec<_> = results.into_iter().flat_map(|r| r.1).collect();
         assert_eq!(matches.len(), 2);
-        let match_labels: Vec<String> = matches.into_iter().map(|mtch| mtch.match_label.clone()).sorted().collect();
+        let match_labels: Vec<String> = matches
+            .into_iter()
+            .map(|mtch| mtch.match_label.clone())
+            .sorted()
+            .collect();
         assert_eq!(match_labels, vec!["uri:example", "uri:phrase"]);
     }
 }
+
